45344eae684ab926c499166d40940b41be8d14f6
manjukalal <manjukalal1496@gmail.com>
2022-05-23 22:10:01 +0530

    empwage

--- empuc2.java
+++ empuc2.java
@@ -1,12 +1,20 @@
-public class empuc2 {
+
+
+
+
+public class empuc3 {
     public static void main(String[] args) {
-        int  present=1;
-        int hours=8;
+        int fulltime=1;
+        int parttime=0;
         int rateperhour=20;
         int salary=0;
+        int emphrs=0;
         double empcheck = Math.floor(Math.random() * 10) % 2;
-           if (present == empcheck)
-     salary=(rateperhour * hours);
-     System.out.println(salary);
-
+       if (fulltime == empcheck)
+            emphrs=8;
+       else if ( parttime== empcheck)
+             emphrs=4;
+        salary=(rateperhour * emphrs);
+       System.out.println(salary);
     }
+}
