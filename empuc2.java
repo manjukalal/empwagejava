8445aaf857b686c3017e198506b4695e87aaf752
manjukalal <manjukalal1496@gmail.com>
2022-05-24 21:25:06 +0530

    empwage

--- empuc2.java
+++ empuc2.java
@@ -1,27 +1,39 @@
-public class empwageuc4 {
 
 
+
+public class empwageuc4
+{
     public static void main(String[] args) {
-        int rateperhrs= 20;
-        int fulltime= 1;
-        int parttime= 2;
-        int empwage= 0;
+        int rateperhrs = 20;
+        int fulltime = 1;
+        int parttime = 2;
+        int empwage = 0;
         int emphr = 0;
-        int empcheck = (int)Math.floor(Math.random()*10)%3;
-        switch (empcheck)
-        {
-            case 1:
-                emphr = 4;
-                break;
+        int workingdays = 20;
+        int totalwage = 0;
 
-            case 2:
-                emphr = 8;
-                break;
+        for ( int days = 0; days<=workingdays; days++ ) {
+            int empcheck = (int) Math.floor(Math.random() * 10) % 3;
 
-            default:
-                emphr =0;
-                break;
+            switch (empcheck) {
+                case 1:
+                    emphr = 4;
+                    break;
+
+                case 2:
+                    emphr = 8;
+                    break;
+
+                default:
+                    emphr = 0;
+                    break;
+            }
+            empwage = (emphr * rateperhrs);
+            totalwage = empwage;
+            System.out.println(empwage);
         }
-        empwage =  ( emphr * rateperhrs );
-        System.out.println ( empwage );
+            System.out.println(totalwage);
+
+
     }
+}
