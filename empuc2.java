30603a69615ddfabb289271065e594a572e4f07f
manjukalal <104911815+manjukalal@users.noreply.github.com>
2022-06-02 21:48:09 +0530

    Update empuc2.java

--- empuc2.java
+++ empuc2.java
@@ -1,16 +1,24 @@
 
 import java.util.*;
-public class cartesianuc2 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter length of first line");
-        int x1 = sc.nextInt();
-        System.out.print("Enter length of  second line");
-        int x2 = sc.nextInt();
-        if (x1 == x2)
-            System.out.println("both lines are equal");
-        else
-            System.out.println("both lines are not equal");
-    }
-}
 
+public class CompareLine {
+        public static final int x1=2,x2=1,y1=4,y2=1;
+        public static final int a1=2,a2=1,b1=4,b2=1;
+
+        public static void main(String[] args)
+        {
+
+            double line1=Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)*1);
+            double line2=Math.sqrt(Math.pow(a2 - a1, 2) + Math.pow(b2 - b1, 2)*1);
+
+            System.out.println("length of line1 is"+line1);
+            System.out.println("length of line2 is"+line2);
+
+           if (line1 == line2){
+               System.out.println("both lines are equals");
+           }
+
+
+    }
+
+}
